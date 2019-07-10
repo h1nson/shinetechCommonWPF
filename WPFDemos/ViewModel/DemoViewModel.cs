78be67c19eb41e99cd48d76965b5729a02deceb2
hinson <twotuli_software@hotmail.com>
2019-07-10 16:06:40 +0800

    init

---
+++ WPFDemos/ViewModel/DemoViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace WPFDemos.ViewModel
+{
+    public class DemoViewModel :ViewModelBase
+    {
+        public RelayCommand SwitchDataContextCommand { get; set; }
+        public RelayCommand DataTemplateCommand { get; set; }
+        public RelayCommand ControlTemplateCommand { get; set; }
+        public RelayCommand ConverterCommand { get; set; }
+        public RelayCommand ResourceCommand { get; set; }
+        public RelayCommand BindingCommand { get; set; }
+        public RelayCommand CustomControlCommand { get; set; }
+        public RelayCommand TreeViewCommand { get; set; }
+        public RelayCommand ValidationCommand { get; set; }
+        public RelayCommand PdfViewerCommand { get; set; }
+        public DemoViewModel ()
+        {
+            SwitchDataContextCommand = new RelayCommand(ShowSwitchDataContextWindow);
+            DataTemplateCommand = new RelayCommand(ShowDataTemplateWindow);
+            ControlTemplateCommand = new RelayCommand(ShowControlTemplateWindow);
+            ConverterCommand = new RelayCommand(ShowConverterWindow);
+            ResourceCommand = new RelayCommand(ShowResourceWindow);
+            BindingCommand = new RelayCommand(ShowBindingWindow);
+            CustomControlCommand = new RelayCommand(ShowCustomControlWindow);
+            TreeViewCommand = new RelayCommand(ShowTreeViewWindow);
+            ValidationCommand = new RelayCommand(ShowValidationWindow);
+            PdfViewerCommand = new RelayCommand(ShowPdfViewerWindow);
+        }
+
+        public void ShowSwitchDataContextWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowSwitchDataContextWindow"));
+        }
+
+        public void ShowDataTemplateWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowDataTemplateWindow"));
+        }
+
+        public void ShowControlTemplateWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowControlTemplateWindow"));
+        }
+
+        public void ShowConverterWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowConverterWindow"));
+        }
+
+        public void ShowResourceWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowResourceWindow"));
+        }
+
+        public void ShowBindingWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowBindingWindow"));
+        }
+
+        public void ShowCustomControlWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowCustomControlWindow"));
+        }
+
+        public void ShowTreeViewWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowTreeViewWindow"));
+        }
+
+        public void ShowValidationWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowValidationWindow"));
+        }
+
+        public void ShowPdfViewerWindow ()
+        {
+            Messenger.Default.Send(new NotificationMessage("ShowPdfViewerWindow"));
+        }
+    }
+}
---
+++ WPFDemos/Views/Demo.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace WPFDemos.Views
+{
+    /// <summary>
+    /// Interaction logic for Demo.xaml
+    /// </summary>
+    public partial class Demo :Window
+    {
+        public Demo ()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<NotificationMessage>(this,NotificationMessageReceived);
+
+            Unloaded += Demo_Unloaded;
+        }
+
+        private void Demo_Unloaded (object sender,RoutedEventArgs e)
+        {
+            Messenger.Default.Unregister<NotificationMessage>(this);
+        }
+
+        private void NotificationMessageReceived (NotificationMessage msg)
+        {
+            Window view = null;
+            switch(msg.Notification)
+            {
+                case "ShowSwitchDataContextWindow":
+                    view = new SwitchDataContext();
+                    view.Show();
+                    break;
+                case "ShowDataTemplateWindow":
+                    view = new DataTemplateView();
+                    view.Show();
+                    break;
+                case "ShowControlTemplateWindow":
+                    view = new ControlTemplateView();
+                    view.Show();
+                    break;
+                case "ShowConverterWindow":
+                    view = new ConverterView();
+                    view.Show();
+                    break;
+                case "ShowResourceWindow":
+                    view = new ResourceView();
+                    view.Show();
+                    break;
+                case "ShowBindingWindow":
+                    view = new BindingView();
+                    view.Show();
+                    break;
+                case "ShowCustomControlWindow":
+                    view = new CustomControlView();
+                    view.Show();
+                    break;
+                case "ShowTreeViewWindow":
+                    view = new TreeViewView();
+                    view.Show();
+                    break;
+                case "ShowValidationWindow":
+                    view = new ValidationView();
+                    view.Show();
+                    break;
+                case "ShowPdfViewerWindow":
+                    view = new PdfViewerView();
+                    view.Show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
